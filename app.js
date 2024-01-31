d8cabf6ef660920c10d2222443483cd3ce589ed9
bdelorean <9021196@student.zadkine.nl>
2024-01-31 13:34:44 +0100

    !

--- app.js
+++ app.js
@@ -11,3 +11,9 @@ window.addEventListener('mousemove', function(e){
     }
     
 })
+const navLinks = document.querySelector('#navLinks');
+const navToggleBtn = document.querySelector('#navToggleBtn');
+
+navToggleBtn.addEventListener ('click', () => { 
+navLinks.classList.toggle ('active');
+});
